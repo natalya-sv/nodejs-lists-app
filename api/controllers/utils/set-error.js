b5ab208048acd199762850adb36324dfac67f52f
Anya <anna.efimova@amo.tech>
2024-09-07 23:53:28 +0200

    fixed format

--- api/controllers/utils/set-error.js
+++ api/controllers/utils/set-error.js
@@ -3,11 +3,11 @@ import { ENTER_VALID_INPUT } from "../../../constants.js";
 
 export function setError(errors) {
   if (!errors.isEmpty()) {
-      const error = new Error(ENTER_VALID_INPUT);
+    const error = new Error(ENTER_VALID_INPUT);
 
-      if (errors.array()[0].type === "field") {
-        error.message = generateFieldValidationErrorMessage(errors.array());
-      }
-      throw new Error(error?.message);
+    if (errors.array()[0].type === "field") {
+      error.message = generateFieldValidationErrorMessage(errors.array());
+    }
+    throw new Error(error?.message);
   }
 }
