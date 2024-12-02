c973614a1fd0c1c097e6b220c250c9b0d8aee071
Natalya Suslova <natalya.suslova@gmail.com>
2024-12-02 15:22:01 +0100

    move test data

---
+++ test-data.js
@@ -0,0 +1,99 @@
+export const categories = [
+  {
+    title: "Cooking",
+    icon: "food",
+    color: "red",
+  },
+  {
+    title: "Reading",
+    icon: "book",
+    color: "blue",
+  },
+  {
+    title: "Travelling",
+    icon: "airplane",
+    color: "green",
+  },
+  {
+    title: "Learning",
+    icon: "computer",
+    color: "orange",
+  },
+  {
+    title: "Meeting",
+    icon: "meet",
+    color: "red",
+  },
+];
+export const subcategories = [
+  {
+    title: "Birthday Cooking",
+  },
+  {
+    title: "Books in English",
+  },
+  {
+    title: "Travelling 2025",
+  },
+  {
+    title: "Learning Vue",
+  },
+  {
+    title: "Meeting meetup",
+  },
+];
+
+export const subcategoryItemsOne = [
+  {
+    title: "5 eggs",
+    description: "description",
+  },
+  {
+    title: "450g flower",
+    description: "description",
+  },
+  {
+    title: "puur chocolate",
+    description: "description",
+  },
+];
+export const subcategoryItemsTwo = [
+  {
+    title: "Alice in the Wonderland",
+    description: "description",
+  },
+  {
+    title: "Leave the world behind",
+    description: "description",
+  },
+  {
+    title: "Nevel let me go",
+    description: "description",
+  },
+  {
+    title: "It ends with us",
+    description: "description",
+  },
+  {
+    title: "Total recall",
+    description: "description",
+  },
+];
+export const subcategoryItemsThree = [
+  {
+    title: "Autralia",
+    description: "description",
+  },
+  {
+    title: "Canada",
+    description: "description",
+  },
+  {
+    title: "Poland",
+    description: "description",
+  },
+  {
+    title: "Griece",
+    description: "description",
+  },
+];
